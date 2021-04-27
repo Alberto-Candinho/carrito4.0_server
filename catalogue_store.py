fa87a581dfbad0e706648817fb9ad9dba8339d98
Alberto-Candinho <aeb1999@gmail.com>
2021-04-28 00:13:42 +0200

    WIP: Ofertas e productos a granel

--- catalogue_store.py
+++ catalogue_store.py
@@ -5,6 +5,9 @@ catalogue = pd.read_csv("catalogo_gadis.csv")
 
 def get_categories():
     categories = []
+    categories.append({
+            "nome": "Ofertas",
+    })
     for category in catalogue["categoria"]:
         if not any(category_dict['nome'] ==  category for category_dict in categories):
             category_dict = {
@@ -14,15 +17,24 @@ def get_categories():
     return categories
 
 def get_products(category):
-    subcatalogue = catalogue[catalogue["categoria"] == category]
-    return subcatalogue.to_dict(orient="records") 
+    if(category != "Ofertas"):
+        subcatalogue = catalogue[catalogue["categoria"] == category]
+        subcatalogue = subcatalogue.sort_values(by=["desconto"], ascending=False)
+        return subcatalogue.to_dict(orient="records") 
+    else:
+        subcatalogue = catalogue[catalogue["desconto"] != 0]
+        subcatalogue = subcatalogue.sort_values(by=["desconto"], ascending=False)
+        return subcatalogue.to_dict(orient="records")
 
 def get_id(id_number):
     id_info = catalogue[catalogue["id"] == int(id_number)]
     return id_info.to_dict(orient="records") 
 
 def has_category(category):
-    return (catalogue["categoria"] == category).any()
+    if(category != "Ofertas"):
+        return (catalogue["categoria"] == category).any()
+    else:
+        return True
 
 def has_id(id_number):
     return (catalogue["id"] == int(id_number)).any()
--- catalogue_test.py
+++ catalogue_test.py
@@ -2,6 +2,10 @@ from flask import jsonify
 import pandas as pd
 import catalogue_store
 
-product_info = catalogue_store.get_id(1)
-if(catalogue_store.has_id(1)):
-    print(product_info)
+#product_info = catalogue_store.get_id(1)
+#if(catalogue_store.has_id(1)):
+    #print(product_info)
+
+#categories = catalogue_store.get_categories()
+products = catalogue_store.get_products("Ofertas")
+print(products)
